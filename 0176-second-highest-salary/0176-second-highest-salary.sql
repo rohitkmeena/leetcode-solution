05a9523421bd3285712771c6f7dff7fedef7e76c
Meena <rohitkmeena30@gmail.com>
2026-07-18 10:34:13 +0530

    Time: 244 ms (99.49%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,10 +1,7 @@
 # Write your MySQL query statement below
-SELECT IFNULL(
-    (
+SELECT(
     Select distinct Salary 
     from Employee
     order by salary Desc
     limit 1 offset 1
-    ),
-    NULL
 ) AS SecondHighestSalary ;
